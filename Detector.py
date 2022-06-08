e050a85b74f2dda98d32e03d48f4c28009f66351
efs <fefsdf>
2022-06-08 15:16:46 +0200

    added getPrices

--- Detector.py
+++ Detector.py
@@ -9,8 +9,8 @@ new_line = '\n'
 tweet_limit = 250
 endpoints = {
     "fr": {
-        "male": "https://www.lululemon.fr/fr-fr/c/hommes/collections/on-en-a-trop-pour-hommes?sz=300",
-        "female": "https://www.lululemon.fr/fr-fr/c/femmes/collections/on-en-a-trop?sz=300"
+        "male": "https://www.lululemon.fr/fr-fr/c/hommes/collections/on-en-a-trop-pour-hommes?sz=400",
+        "female": "https://www.lululemon.fr/fr-fr/c/femmes/collections/on-en-a-trop?sz=400"
     }
 }
 
@@ -20,6 +20,8 @@ class WMTM:
         self.country = country
         self.endpoint = endpoints[country]
 
+    # compute new products that were recently added to WMTM for a specific gender
+    # return: ([{}], [{}])
     def getItems(self, gender):
         fileName = f'items_{self.country}_{gender}.json'
         html_content = requests.get(self.endpoint[gender]).text
@@ -29,7 +31,7 @@ class WMTM:
         current_items = []
         for item in items:
             itemName = ' '.join(item.find("img")["alt"].replace('"', 'inch').split()) # remove non breaking space
-            sizes = self.getSizes(item)
+            sizes = self.getSizes(str(item))
             current_items.append(f"{itemName} ({sizes})")
         current_items = set(current_items)
 
@@ -43,15 +45,19 @@ class WMTM:
         new_items = current_items.difference(old_items)
         return new_items, current_items
 
+    # get messages for new products
+    # return: [string]
     def formatMessages(self, new_items, gender):
         header = f"Nouveau produits {'hommes' if gender=='male' else 'femmes'}:{new_line}"
         splitted_message = []
         message = ""
+        j = 0
         for i, item in enumerate(new_items):
             if (len(message + f'{new_line}{item}'))>tweet_limit:
-                splitted_message.append(f"{message}{new_line}{i}")
+                splitted_message.append(f"{message}{new_line}{j}")
                 message = ""
             message += f'{new_line}{item}'
+            j+=1
         splitted_message.append(f"{message}{new_line}")
         splitted_message[0] = header + splitted_message[0]
         return splitted_message
@@ -76,17 +82,25 @@ class WMTM:
         else:
             print("no new items")
 
-    def getSizes(self, item):
-        itemStr = str(item)
-        itemRoot = lxml.html.fromstring(itemStr)
+    # itemHTML: string (from product's html page)
+    # return: string (product's available sizes)
+    def getSizes(self, itemHTML):
+        itemRoot = lxml.html.fromstring(itemHTML)
         itemURL = itemRoot.xpath("//div[@class='image-container']/a[@data-lulu-attributes]/@href")[0]
         itemContent = requests.get(itemURL).text
         itemRoot = lxml.html.fromstring(itemContent)
-        sizes = itemRoot.xpath("//div[@data-attr='size']//div[@class='custom-select-btn ']/span[input[not(@disabled)]]/label/text()")
-        if (sizes[0] == "ONE SIZE"):
+        sizes = itemRoot.xpath("//@data-availablesizes")[0]
+        if (sizes == "ONE SIZE"):
             return ""
         else:
-            return ','.join(sizes)
+            return sizes
+
+    # return old price and reduced price from product's html
+    def getPrices(self, itemHTML):
+        itemRoot = lxml.html.fromstring(itemHTML)
+        newPrice = itemRoot.xpath("//span[@class='cta-price-value'][span[@class='list-price'] and span[@class='markdown-prices']]/span[@class='markdown-prices']/text()")
+        oldPrice = itemRoot.xpath("//span[@class='cta-price-value'][span[@class='list-price'] and span[@class='markdown-prices']]/span[@class='list-price']/text()")
+        return oldPrice, newPrice
 
 if __name__=="__main__":
     wmtm = WMTM("fr")
